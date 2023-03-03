b17b6b361727a60f785604a245d232150922b119
Taras <topanasenko85@gmail.com>
2023-03-03 21:56:28 +0200

    updat

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const generateHtml = ({ description, original, preview }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+};
+
+const generateGallery = galleryItems.map(generateHtml).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', generateGallery);
+
+// **//*** */
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log(
+    player.getCurrentTime().then(function (seconds) {
+      // seconds = the current playback position
+      console.log(seconds);
+    })
+  );
+});
+
+player
+  .getCurrentTime()
+  .then(function (seconds) {
+    // seconds = the current playback position
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    // an error occurred
+  });
+
+player
+  .setCurrentTime(230)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+function getTimeUpdate(data) {
+  console.log(data);
+}
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});

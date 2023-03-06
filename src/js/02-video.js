a091e5e88e1a79acf40928ade5c8296dff6a7574
Taras <topanasenko85@gmail.com>
2023-03-06 20:37:21 +0200

    video full

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,13 +7,15 @@ const player = new Player(iframe);
 const onPlay = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 
-  console.log(data.seconds);
+  // console.log(data.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 player.setColor('#FF6347').then(function (color) {
   console.log('#D8E0FF');
 });
+player.setVolume(10);

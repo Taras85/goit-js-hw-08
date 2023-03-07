7c20f161e163d05d606b9d2cd73e4b2b409486aa
Taras <102531278+Taras85@users.noreply.github.com>
2023-03-07 09:38:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,16 +6,16 @@ const player = new Player(iframe);
 
 const onPlay = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
-
-  // console.log(data.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-if (localStorage.getItem('videoplayer-current-time')) {
+if (localStorage.getItem('videoplayer-current-time') !== null) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  console.log(localStorage.getItem('videoplayer-current-time') !== null);
 }
+
 player.setColor('#FF6347').then(function (color) {
   console.log('#D8E0FF');
 });
-player.setVolume(10);
+// player.setVolume(10).then(function (volume) {});
